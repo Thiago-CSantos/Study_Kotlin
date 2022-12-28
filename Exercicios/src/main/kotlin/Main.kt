dc51986d1dde88332896c8466e5ac7ffe151cfe6
Thiago C. Santos <thiagocarretero433@gmail.com>
2022-12-27 23:04:30 -0300

    Hello World e primeiro exercicio

---
+++ Exercicios/src/main/kotlin/Main.kt
@@ -0,0 +1,36 @@
+fun main(args: Array<String>) {
+    /*
+        * a. Crie um novo arquivo com uma função main.
+        * b. Declare uma variável mutável capaz de armazenar seu nome completo.
+        * c. Declare uma variável de texto sem valor algum.
+        * d. Declare uma variável imutável com o menor tipo de dado possível capaz de armazenar o número que você calça.
+        * e. Declare uma variável capaz de armazenar o PIB do Brasil (1.869.000.000.000).
+        * f. Declare uma variável capaz de armazenar a população do Brasil (211.000.000).
+        * g. Imprima o valor do PIB per capita.
+        * h. Rode seu programa de maneira que não tenha erros de compilação ou execução.
+      **/
+
+    // b)
+    var nome: String = "Thiago Carretero dos Santos"
+    println(nome)
+
+    // c)
+    var texto: String
+
+    // d)
+    val numCalçado: Byte = 39
+    println(numCalçado)
+
+    // e)
+    val PIB: Long = 1609000000000
+    println(PIB)
+
+    // f)
+    val populacao: Int = 213993441
+    println(populacao)
+
+    // g)
+    val PIB_perCapita: Double = 254360000000.0
+    println("PIB 3º trimestre de 2022 $PIB_perCapita")
+
+}
---
+++ HelloWorld/src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    println("Hello, World!")
+    println("Ola! mundo")
+
+    var nome: String = "Thiago"
+    var sobrenome = "Santos"
+
+
+    println()
+}
